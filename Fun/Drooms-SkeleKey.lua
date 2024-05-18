b404ff4c0404dda40bd0d8d94f69f510c947e8f1
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-05-18 06:57:45 -0700

    Create Drooms-SkeleKey.lua

---
+++ Fun/Drooms-SkeleKey.lua
@@ -0,0 +1,5 @@
+local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
+
+local Tool = ServerStorage.SkeletonKey:Clone()
+Tool.Parent = Players.TotallyNotChrono.Backpack
