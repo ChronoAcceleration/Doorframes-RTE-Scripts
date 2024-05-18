fb014e4a98abe7f3722490d5482d981743d38df3
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-05-18 06:49:47 -0700

    Create Drooms-CrucifixGrab.lua

---
+++ Fun/Drooms-CrucifixGrab.lua
@@ -0,0 +1,7 @@
+--!server
+
+local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
+
+local Tool = ServerStorage.Crucifix:Clone()
+Tool.Parent = Players.TotallyNotChrono.Backpack
