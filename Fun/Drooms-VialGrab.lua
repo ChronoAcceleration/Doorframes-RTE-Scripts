d6039eac4eafad79c32de1d40464b16343d1d2bb
Chrono <150987804+ChronoAcceleration@users.noreply.github.com>
2024-05-18 06:53:32 -0700

    Update Drooms-VialGrab.lua

--- Fun/Drooms-VialGrab.lua
+++ Fun/Drooms-VialGrab.lua
@@ -1,9 +1,4 @@
-for i = 1, 11 do
-	local Players = game:GetService("Players")
-	local ServerStorage = game:GetService("ServerStorage")
-
-	local Tool = ServerStorage.StarVial:Clone()
-	Tool.Parent = Players.TotallyNotChrono.Backpack
-	
-	task.wait()
-end
+local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
+local Tool = ServerStorage.StarVial:Clone()
+Tool.Parent = Players.TotallyNotChrono.Backpack
